88d3b8d6d88e4c778a70c9c4ca8510a0ad3b1da8
Eclectikus <joanhxxi@gmail.com>
2020-08-27 19:52:48 +0200

    Update Task.stories.js

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -4,12 +4,15 @@ import React from 'react';
 import {
     action
 } from '@storybook/addon-actions';
-
 import Task from './Task';
-
+import {
+    withKnobs,
+    object
+} from '@storybook/addon-knobs';
 export default {
     component: Task,
     title: 'Task',
+    decorators: [withKnobs],
     // Our exports that end in "Data" are not stories.
     excludeStories: /.*Data$/,
 };
