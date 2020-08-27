c2c7a6e3f6406d814e48ab5643b4643720417312
Eclectikus <joanhxxi@gmail.com>
2020-08-27 18:52:01 +0200

    Complemento Knob de Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,9 +5,11 @@ module.exports = {
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
+    '@storybook/addon-knobs',
     '@storybook/addon-links',
   ],
 };
+
 
 
 // module.exports = {
